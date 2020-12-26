43021c412ec485e95cc33a6427e5be201743e5e0
Elynn <elynnlee@utexas.edu>
2020-12-26 11:53:54 -0800

    add card.module.css

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,11 +1,20 @@
 import * as React from "react";
+import { Card } from "../model/card";
 
 const Test: React.FC = () => {
-  return (
-    <>
-      <div>Test</div>
-    </>
-  );
+	var name = "POSTAL_PIGEON";
+	var card = Card.fromName(name);
+	var cost = card.baseCost; // [ResourceType.BERRY]: 2,
+
+	return (
+		<>
+			<div>Testing</div>
+			{card.name}
+			<div></div>
+			{JSON.stringify(card.baseCost, null, 2)}
+			<pre>{JSON.stringify(card, null, 2)}</pre>
+		</>
+	);
 };
 
 export default Test;

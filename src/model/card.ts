0ebb51922ca1ec917f78be0c17e02de0d74caba6
Michael Yong <wrong92@gmail.com>
2020-12-26 09:48:08 -0800

    add card registry

--- src/model/card.ts
+++ src/model/card.ts
@@ -1,16 +1,57 @@
 import { ResourceType, CardCost, CardType, CardName } from "./types";
 
-export const allCards = [
-  {
-    name: "Postal Pigeon",
+export class Card {
+  readonly name: CardName;
+  readonly baseCost: CardCost;
+  readonly baseVP: number;
+  readonly cardType: CardType;
+  readonly isUnique: boolean;
+  readonly isCritter: boolean;
+  readonly isConstruction: boolean;
+  readonly associatedCard: CardName;
+
+  constructor({
+    name,
+    baseCost,
+    baseVP,
+    cardType,
+    isUnique,
+    isConstruction,
+    associatedCard,
+  }: {
+    name: CardName;
+    baseCost: CardCost;
+    baseVP: number;
+    cardType: CardType;
+    isUnique: boolean;
+    isConstruction: boolean;
+    associatedCard: CardName;
+  }) {
+    this.name = name;
+    this.baseCost = baseCost;
+    this.baseVP = baseVP;
+    this.cardType = cardType;
+    this.isUnique = isUnique;
+    this.isCritter = !isConstruction;
+    this.isConstruction = isConstruction;
+    this.associatedCard = associatedCard;
+  }
+
+  static fromName(name: CardName): Card {
+    return CARD_REGISTRY[name];
+  }
+}
+
+const CARD_REGISTRY: Record<string /* TODO replace with CardName */, Card> = {
+  [CardName.POSTAL_PIGEON]: new Card({
+    name: CardName.POSTAL_PIGEON,
     cardType: CardType.TRAVELER,
-    rawCost: {
+    baseCost: {
       [ResourceType.BERRY]: 2,
     },
+    baseVP: 0,
     isUnique: false,
-    isCritter: true,
     isConstruction: false,
-    associatedCard: "Post Office",
-    getPointTotal: () => 0,
-  },
-];
+    associatedCard: CardName.POST_OFFICE,
+  }),
+};

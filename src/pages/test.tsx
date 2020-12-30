702d9d052304fd295e94d9ed00e61e76f206fb3d
Elynn <elynnlee@utexas.edu>
2020-12-30 15:49:22 -0800

    add game input box for worker placement to locations + styling

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -2,18 +2,33 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Test from "../components/Test";
 import Card from "../components/Card";
-import { CardName } from "../model/types";
+import Location from "../components/Location";
+import { CardName, LocationName } from "../model/types";
 
 export default function TestPage() {
-  var renderAllCards = true;
-  var cardsToRender = [];
+  const renderAllCards = true;
+  let cardsToRender: CardName[] = [];
+
+  const renderAllLocations = true;
+  let locationsToRender: LocationName[] = [];
 
   if (renderAllCards == true) {
-    var enums = Object.keys(CardName) as CardName[];
+    const enums = Object.keys(CardName) as CardName[];
     cardsToRender.push(...enums);
   } else {
     cardsToRender = [CardName.POSTAL_PIGEON];
   }
+
+  if (renderAllLocations == true) {
+    const locations = Object.keys(LocationName) as LocationName[];
+    locationsToRender.push(...locations);
+  } else {
+    locationsToRender = [
+      LocationName.BASIC_ONE_BERRY,
+      LocationName.FOREST_COPY_BASIC_ONE_CARD,
+    ];
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -22,6 +37,11 @@ export default function TestPage() {
       <div>
         {cardsToRender.map(function (cardsToRender, index) {
           return <Card key={index} name={cardsToRender} />;
+        })}
+      </div>
+      <div>
+        {locationsToRender.map(function (locationsToRender, index) {
+          return <Location key={index} name={locationsToRender} />;
         })}
       </div>
     </div>

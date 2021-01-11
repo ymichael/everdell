3a1ef04c8ef4fed330467cae8c95d826c24401bd
Michael Yong <wrong92@gmail.com>
2021-01-10 20:34:36 -0800

    Fix misleading string

--- src/components/GameInputSelectPaymentForCard.tsx
+++ src/components/GameInputSelectPaymentForCard.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
 import { useRef } from "react";
 
-import { GameInputSelectPaymentForCard as TGameInputSelectPaymentForCard } from "../model/types";
+import {
+  GameInputSelectPaymentForCard as TGameInputSelectPaymentForCard,
+  LocationName,
+  CardName,
+} from "../model/types";
 import { Player } from "../model/player";
 
 import { ResourcesForm } from "./CardPayment";
@@ -13,7 +17,15 @@ const GameInputSelectPaymentForCard: React.FC<{
 }> = ({ name, gameInput, viewingPlayer }) => {
   return (
     <>
-      <p>Play for {gameInput.card}:</p>
+      <p>
+        {`Play ${gameInput.card}`}
+        {gameInput.locationContext ===
+        LocationName.FOREST_DRAW_TWO_MEADOW_PLAY_ONE_FOR_ONE_LESS
+          ? " for 1 fewer resource"
+          : gameInput.cardContext === CardName.INN
+          ? " for 3 fewer resources"
+          : ""}
+      </p>
       <ResourcesForm name={name} />
     </>
   );

a9e594289401c6e395182daf5a6e1dd17e125881
Michael Yong <wrong92@gmail.com>
2021-01-10 16:00:08 -0800

    More test input types

--- src/pages/test/inputs.tsx
+++ src/pages/test/inputs.tsx
@@ -15,7 +15,7 @@ import GameInputBox from "../../components/GameInputBox";
 import { testInitialGameState } from "../../model/testHelpers";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const playerNames = ["Michael", "Elynn"];
+  const playerNames = ["Michael", "Elynn", "Chris", "Vanessa"];
   const numPlayers = playerNames.length;
   let gameState = testInitialGameState({
     numPlayers,
@@ -30,6 +30,11 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
     player.drawCards(gameState, idx);
     player.cardsInHand.push(CardName.RANGER);
+    player.cardsInHand.push(CardName.FOOL);
+    player.cardsInHand.push(CardName.INNKEEPER);
+    player.cardsInHand.push(CardName.WANDERER);
+    player.cardsInHand.push(CardName.KING);
+    player.cardsInHand.push(CardName.POSTAL_PIGEON);
     player.cardsInHand.push(CardName.BARD);
 
     player.gainResources({
@@ -65,16 +70,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     LocationName.FOREST_DISCARD_ANY_THEN_DRAW_TWO_PER_CARD
   ] = [];
 
-  gameState.meadowCards.push(
-    CardName.FARM,
-    CardName.FARM,
-    CardName.FARM,
-    CardName.FARM,
-    CardName.FARM,
-    CardName.FARM,
-    CardName.FARM,
-    CardName.FARM
-  );
+  gameState.meadowCards.push(CardName.KING, CardName.FARM);
 
   const game = new GameModel({
     gameId: "testGameId",
@@ -82,34 +78,100 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     gameState,
   });
 
-  const player = game.getActivePlayer();
-  const isActivePlayer = true;
   return {
     props: {
-      game: game.toJSON(false /* includePrivate */),
-      viewingPlayer: player.toJSON(true /* includePrivate */),
+      game: game.toJSON(true /* includePrivate */),
     },
   };
 };
 
-export default function TestGameInputPage(props: {
-  game: GameJSON;
-  viewingPlayer: PlayerJSON;
-}) {
-  const { game, viewingPlayer } = props;
+export default function TestGameInputPage(props: { game: GameJSON }) {
+  const { game } = props;
   const { gameId, gameState } = game;
-  gameState.players = gameState.players.map((player) => {
-    if (player.playerId === viewingPlayer.playerId) {
-      return viewingPlayer;
-    } else {
-      return player;
-    }
-  });
-
   const gameStateImpl = GameState.fromJSON(gameState);
   const gameState1 = gameStateImpl.clone();
 
   const gameState2 = gameStateImpl.clone();
+
+  const gameStateDiscardCards = gameStateImpl.next({
+    inputType: GameInputType.PLAY_CARD,
+    clientOptions: {
+      card: CardName.BARD,
+      fromMeadow: false,
+      paymentOptions: {
+        resources: { [ResourceType.BERRY]: 3 },
+      },
+    },
+  });
+
+  const gameStateSelectWorkerPlacement = gameStateImpl.next({
+    inputType: GameInputType.PLAY_CARD,
+    clientOptions: {
+      card: CardName.RANGER,
+      fromMeadow: false,
+      paymentOptions: {
+        resources: { [ResourceType.BERRY]: 2 },
+      },
+    },
+  });
+
+  const gameStateSelectPlayer = gameStateImpl.next({
+    inputType: GameInputType.PLAY_CARD,
+    clientOptions: {
+      card: CardName.FOOL,
+      fromMeadow: false,
+      paymentOptions: {
+        resources: { [ResourceType.BERRY]: 3 },
+      },
+    },
+  });
+
+  const gameStateSelectLocation = gameStateImpl.next({
+    inputType: GameInputType.VISIT_DESTINATION_CARD,
+    clientOptions: {
+      playedCard: gameStateImpl
+        .getActivePlayer()
+        .getFirstPlayedCard(CardName.LOOKOUT),
+    },
+  });
+
+  const gameStateSelectResources = gameStateImpl.next({
+    inputType: GameInputType.PLACE_WORKER,
+    clientOptions: {
+      location: LocationName.FOREST_TWO_WILD,
+    },
+  });
+
+  const gameStateSelectCards = gameStateImpl.next({
+    inputType: GameInputType.PLAY_CARD,
+    clientOptions: {
+      card: CardName.POSTAL_PIGEON,
+      fromMeadow: false,
+      paymentOptions: {
+        resources: { [ResourceType.BERRY]: 2 },
+      },
+    },
+  });
+
+  let gameStatePaymentForCard = gameStateImpl.next({
+    inputType: GameInputType.VISIT_DESTINATION_CARD,
+    clientOptions: {
+      playedCard: gameStateImpl
+        .getActivePlayer()
+        .getFirstPlayedCard(CardName.INN),
+    },
+  });
+  gameStatePaymentForCard = gameStatePaymentForCard.next({
+    inputType: GameInputType.SELECT_CARDS,
+    prevInputType: GameInputType.VISIT_DESTINATION_CARD,
+    cardContext: CardName.INN,
+    cardOptions: gameStatePaymentForCard.meadowCards,
+    maxToSelect: 1,
+    minToSelect: 1,
+    clientOptions: {
+      selectedCards: [CardName.KING],
+    },
+  });
 
   return (
     <>
@@ -176,6 +238,55 @@ export default function TestGameInputPage(props: {
           },
         ]}
         viewingPlayer={gameState2.players[0]}
+      />
+      <hr />
+      <GameInputBox
+        gameId={"testGameId"}
+        gameState={gameStateDiscardCards.toJSON(true)}
+        gameInputs={gameStateDiscardCards.pendingGameInputs}
+        viewingPlayer={gameStateDiscardCards.players[0]}
+      />
+      <hr />
+      <GameInputBox
+        gameId={"testGameId"}
+        gameState={gameStateSelectWorkerPlacement.toJSON(true)}
+        gameInputs={gameStateSelectWorkerPlacement.pendingGameInputs}
+        viewingPlayer={gameStateSelectWorkerPlacement.players[0]}
+      />
+      <hr />
+      <GameInputBox
+        gameId={"testGameId"}
+        gameState={gameStateSelectPlayer.toJSON(true)}
+        gameInputs={gameStateSelectPlayer.pendingGameInputs}
+        viewingPlayer={gameStateSelectPlayer.players[0]}
+      />
+      <hr />
+      <GameInputBox
+        gameId={"testGameId"}
+        gameState={gameStateSelectLocation.toJSON(true)}
+        gameInputs={gameStateSelectLocation.pendingGameInputs}
+        viewingPlayer={gameStateSelectLocation.players[0]}
+      />
+      <hr />
+      <GameInputBox
+        gameId={"testGameId"}
+        gameState={gameStateSelectResources.toJSON(true)}
+        gameInputs={gameStateSelectResources.pendingGameInputs}
+        viewingPlayer={gameStateSelectResources.players[0]}
+      />
+      <hr />
+      <GameInputBox
+        gameId={"testGameId"}
+        gameState={gameStateSelectCards.toJSON(true)}
+        gameInputs={gameStateSelectCards.pendingGameInputs}
+        viewingPlayer={gameStateSelectCards.players[0]}
+      />
+      <hr />
+      <GameInputBox
+        gameId={"testGameId"}
+        gameState={gameStatePaymentForCard.toJSON(true)}
+        gameInputs={gameStatePaymentForCard.pendingGameInputs}
+        viewingPlayer={gameStatePaymentForCard.players[0]}
       />
     </>
   );

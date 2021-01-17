e4f285309a1ba3cfc05b3ee26fdb1d9e45d7395a
Michael Yong <wrong92@gmail.com>
2021-01-16 16:34:31 -0800

    Implement PG adapter

--- src/model/db.ts
+++ src/model/db.ts
@@ -1,6 +1,9 @@
-import sqlite3 from "sqlite3";
 import path from "path";
 import fs from "fs";
+
+import sqlite3 from "sqlite3";
+import { Pool } from "pg";
+
 import { GameJSON } from "./jsonTypes";
 
 const pgUrl = process.env.DATABASE_URL;
@@ -20,8 +23,12 @@ const getDbInstance = (): IDb => {
     if (dbPath) {
       _instance = new SqliteDb(dbPath);
     }
-
-    throw new Error("Unable to instantiate DB instance.");
+    if (pgUrl) {
+      _instance = new PgDb(pgUrl);
+    }
+    if (!_instance) {
+      throw new Error("Unable to instantiate DB instance.");
+    }
   }
   return _instance;
 };
@@ -33,6 +40,69 @@ interface IDb {
   updateGame(gameId: string, gameJSON: string): Promise<void>;
   hasSavedGame(gameId: string): Promise<boolean>;
   getGameJSONById(gameId: string): Promise<GameJSON | null>;
+}
+
+class PgDb implements IDb {
+  private pgUrl: string;
+  private pool: Pool;
+
+  constructor(pgUrl: string) {
+    this.pgUrl = pgUrl;
+    this.pool = new Pool({
+      connectionString: this.pgUrl,
+    });
+  }
+
+  async createGamesTableIfNotExists(): Promise<void> {
+    await this.pool.query(
+      `CREATE TABLE IF NOT EXISTS
+        games(
+          game_id varchar,
+          game text,
+          created_time timestamp default now(),
+          PRIMARY KEY (game_id)
+        )`
+    );
+  }
+
+  async saveGame(gameId: string, gameJSON: string): Promise<void> {
+    const hasSavedGame = await this.hasSavedGame(gameId);
+    if (hasSavedGame) {
+      await this.updateGame(gameId, gameJSON);
+    } else {
+      await this.createGame(gameId, gameJSON);
+    }
+  }
+
+  async createGame(gameId: string, gameJSON: string): Promise<void> {
+    await this.pool.query("INSERT INTO games(game_id, game) VALUES($1, $2)", [
+      gameId,
+      gameJSON,
+    ]);
+  }
+
+  async updateGame(gameId: string, gameJSON: string): Promise<void> {
+    await this.pool.query("UPDATE games SET game = $1 WHERE game_id = $2", [
+      gameJSON,
+      gameId,
+    ]);
+  }
+
+  async hasSavedGame(gameId: string): Promise<boolean> {
+    const result = await this.pool.query(
+      "SELECT game_id FROM games WHERE game_id = $1",
+      [gameId]
+    );
+    return result.rows.length === 1;
+  }
+
+  async getGameJSONById(gameId: string): Promise<GameJSON | null> {
+    const result = await this.pool.query(
+      "SELECT game game FROM games WHERE game_id = $1",
+      [gameId]
+    );
+    return result.rows.length === 1 ? JSON.parse(result.rows[0].game) : null;
+  }
 }
 
 class SqliteDb implements IDb {

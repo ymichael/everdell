8bf571930b5967e27f48defde08bd0cfb46939b3
Michael Yong <wrong92@gmail.com>
2021-01-22 17:31:47 -0800

    Collect pending inputs from tests to render on test page.

--- src/model/testHooks.ts
+++ src/model/testHooks.ts
@@ -2,28 +2,44 @@ import fs from "fs";
 import uniqBy from "lodash/uniqBy";
 import { GameState, gameTextToDebugStr } from "./gameState";
 import { toGameText } from "./gameText";
-import { GameText } from "./types";
+import { GameText, GameInputMultiStep } from "./types";
 
-// If specified, we dump the game logs into a local file.
-const shouldDumpGameLogs = process.env.DUMP_GAME_LOGS;
+// If specified, we dump the game test data into a local file.
+const shouldDumpGameTestData = process.env.DUMP_GAME_TEST_DATA;
 
 let origAddGameLog: any;
+let origNext: any;
+const pendingGameInputs: GameInputMultiStep[][] = [];
 const gameTextArr: GameText[] = [];
 
 export const mochaHooks = {
   beforeAll() {
-    if (shouldDumpGameLogs) {
+    if (shouldDumpGameTestData) {
       origAddGameLog = GameState.prototype.addGameLog;
       GameState.prototype.addGameLog = function (...args) {
         gameTextArr.push(toGameText(...args));
         return origAddGameLog.call(this, ...args);
       };
+
+      origNext = GameState.prototype.next;
+      GameState.prototype.next = function (...args) {
+        const ret = origNext.call(this, ...args);
+        if (ret.pendingGameInputs.length !== 0) {
+          pendingGameInputs.push(...ret.pendingGameInputs);
+        }
+        return ret;
+      };
     }
   },
 
   afterAll() {
-    if (shouldDumpGameLogs) {
+    if (shouldDumpGameTestData) {
       GameState.prototype.addGameLog = origAddGameLog;
+      GameState.prototype.next = origNext;
+      fs.writeFileSync(
+        "./src/pages/test/pendingGameInputs.json",
+        JSON.stringify(pendingGameInputs, null, 2)
+      );
       fs.writeFileSync(
         "./src/pages/test/logs.json",
         JSON.stringify(uniqBy(gameTextArr, gameTextToDebugStr), null, 2)
---
+++ src/pages/test/inputBoxLabel.tsx
@@ -0,0 +1,28 @@
+import { GetServerSideProps } from "next";
+
+import GameLog from "../../components/GameLog";
+import { GameInputMultiStep } from "../../model/types";
+import { renderGameInputLabel } from "../../components/gameInputCommon";
+import pendingGameInputsJSON from "./pendingGameInputs.json";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return {
+    props: {
+      pendingGameInputsJSON,
+    },
+  };
+};
+
+export default function TestGamePendingInputsPage(props: {
+  pendingGameInputsJSON: GameInputMultiStep[];
+}) {
+  return props.pendingGameInputsJSON.map(
+    (gameInput: GameInputMultiStep, idx: number) => {
+      return (
+        <div style={{ padding: "5px" }} key={idx}>
+          {renderGameInputLabel(gameInput)}
+        </div>
+      );
+    }
+  );
+}

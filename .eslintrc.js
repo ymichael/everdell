f734ffda3d40d8101e0eb28e6ccd009b684b68ff
Michael Yong <wrong92@gmail.com>
2021-01-24 20:53:20 -0800

    Disable @typescript-eslint/no-inferrable-types lint rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   rules: {
+    "@typescript-eslint/no-inferrable-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/ban-types": "off",
     "@typescript-eslint/no-unused-vars": [

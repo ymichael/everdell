0d69fa730392a1f3a99c1ecdc9a44d2817e9e906
Elynn Lee <elynnlee@utexas.edu>
2022-12-03 20:33:46 -0800

    boilerplate visitor tests

--- src/model/visitor.test.ts
+++ src/model/visitor.test.ts
@@ -34,4 +34,159 @@ describe("Visitor", () => {
       expect(player.getPointsFromVisitors(gameState)).to.be(7);
     });
   });
+  describe.skip(VisitorName.BOSLEY_TEDWARDSON, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.BUTTERBELL_SWEETPAW, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.DIGGS_DEEPWELL, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.DILLWEED_QUICKSNIFF, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.DIM_DUSTLIGHT, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.DIP_DUBBLE, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.DUNE_TARRINGTON, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.DWELL_NORTHWATCH, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.EDVARD_TRIPTAIL, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.FRIN_STICKLY, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.GLINDIL_FRINK, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.IGGY_SILVERSCALE, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.MOSSY_STEPTOE, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.ORIN_NIMBLEPAW, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.OSCAR_LONGTALE, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.PHILL_GURGLE, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.PIFF_QUILLGLOW, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.PLUM_SHORTCLAW, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.QUINN_CLEANWHISKER, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.REEMY_SNIGGLE, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.RIVIL_ABLACUS, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.RUBY_DEW, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.SARIS_CLEARWHISTLE, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.SIR_TRIVLE_Q_S_MARQWILL_III, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.SKIN_SHINYSNOUT, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.SNOUT_PUDDLEHOP, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.TRISS_PESKE, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.VARA_AND_BRUN_MAYBERRY, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.WILDELL_FAMILY, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.WILLOW_GREENGRIN, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
+  describe.skip(VisitorName.WIMBLE_WUFFLE, () => {
+    it("gives correct number of points when claimed", () => {
+      expect(false);
+    });
+  });
 });

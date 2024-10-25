ceabdbb45481dc56a190a92c4f9903a4a9a3060b
Sergei Stepanov <sergei.dev@outlook.com>
2024-10-25 16:04:03 +0200

    Make the status bar collapsible

--- src/components/StickyBar.tsx
+++ src/components/StickyBar.tsx
@@ -1,4 +1,7 @@
 import * as React from "react";
+import { GameState } from "../model/gameState";
+import { Player } from "../model/player";
+import { ResourceType } from "../model/types";
 import styles from "../styles/Game.module.css"; // Import your Game component's styles
 import {
   CardTypeList,
@@ -6,9 +9,7 @@ import {
   PlayerName,
   ResourceList,
 } from "./Players";
-import { ResourceType } from "../model/types";
-import { Player } from "../model/player";
-import { GameState } from "../model/gameState";
+import { BsChevronDoubleDown } from "react-icons/bs";
 
 export const StickyBar: React.FC<{
   playerNames: string[];
@@ -16,28 +17,56 @@ export const StickyBar: React.FC<{
   players: Player[];
   gameState: GameState;
 }> = ({ playerNames, playerResources, players, gameState }) => {
-  return (
-    <div className={styles.stickyBar}>
-      {playerNames.map((name) => {
-        return (
-          <div className={styles.sticky_bar_item}>
-            <PlayerName
-              name={name}
-              onClick={function (): void {
-                throw new Error("Function not implemented.");
-              }}
-            />
-            <ResourceList playerResources={playerResources[name]} />
-            <CardTypeList player={players.find((p) => p.name === name)!} />
+  const [isCollapsed, setIsCollapsed] = React.useState(false);
 
-            <OtherResources
-              player={players.find((p) => p.name === name)!}
-              gameState={gameState}
-              showRealtimePoints={true}
-            />
-          </div>
-        );
-      })}
+  const handleClick = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+  return (
+    <div className={styles.stickyBarContainer}>
+      <CollapseButton isCollapsed={isCollapsed} handleClick={handleClick} />
+      <div
+        className={`${styles.stickyBar} ${isCollapsed ? styles.hidden : ""}`}
+      >
+        <div className={styles.stickyBarContent}>
+          {playerNames.map((name) => {
+            return (
+              <div key={name} className={styles.sticky_bar_item}>
+                <PlayerName
+                  name={name}
+                  onClick={function (): void {
+                    throw new Error("Function not implemented.");
+                  }}
+                />
+                <ResourceList playerResources={playerResources[name]} />
+                <CardTypeList player={players.find((p) => p.name === name)!} />
+
+                <OtherResources
+                  player={players.find((p) => p.name === name)!}
+                  gameState={gameState}
+                  showRealtimePoints={true}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const CollapseButton: React.FC<{
+  isCollapsed: boolean;
+  handleClick: () => void;
+}> = ({ isCollapsed, handleClick }) => {
+  return (
+    <div
+      className={`${styles.collapseButton} ${
+        isCollapsed ? styles.clicked : ""
+      }`}
+      onClick={handleClick}
+    >
+      <BsChevronDoubleDown size="48" />
     </div>
   );
 };
